0219f1cbe6df72f1a8e40ecbbab4bda44f1067c6
Andre Christoga <andrexchristoga@gmail.com>
2018-07-24 11:04:39 +0700

    Add back vuepress dir

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'musik',
+  description: `Andre Sion's music collection`,
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about' },
+      { text: 'Repo', link: 'https://github.com/caxvis/musik' },
+      { text: 'Author', link: 'https://andre.js.org' }
+    ]
+  }
+}
